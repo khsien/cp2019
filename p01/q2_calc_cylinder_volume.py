cff97e507f896ca4976a7411f58d0fc73a0603c3
khsien <47766393+khsien@users.noreply.github.com>
2019-02-24 15:42:36 +0800

    Update q2_calc_cylinder_volume.py

--- p01/q2_calc_cylinder_volume.py
+++ p01/q2_calc_cylinder_volume.py
@@ -1,1 +1,12 @@
 
+radius = int(input("Enter Radius Here:"))
+
+length = int(input("Enter Length Here:"))
+
+from math import pi
+
+area = radius ** 2 * pi
+
+volume = area * length
+
+print("Volume is " + str(volume))
