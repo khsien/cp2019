bccf4eda914d0840bd3ea485e2f876b1cfd06057
khsien <47766393+khsien@users.noreply.github.com>
2019-03-07 20:28:46 +0800

    Update q07_miles_to_kilometres.py

--- p02/q07_miles_to_kilometres.py
+++ p02/q07_miles_to_kilometres.py
@@ -1,1 +1,10 @@
 
+def mlstokm(miles):
+    km = miles * 1.609
+    return "{0:.3f}".format(km)
+def kmtomls(kilometres):
+    miles = kilometres/1.609
+    return "{0:.3f}".format(miles)
+print("Miles",'\t','Kilometres','\t',"Kilometres",'\t',"Miles")
+for i in range(1,11):
+    print(str(i) + '\t ' + mlstokm(i) + '\t         ' + str(i*5+15) + '\t         ' + kmtomls(i*5+15))
