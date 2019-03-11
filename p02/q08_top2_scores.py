fa94eb3a6efb89069193c6fcabf75f144bac18b5
khsien <47766393+khsien@users.noreply.github.com>
2019-03-11 16:21:34 +0800

    Update q08_top2_scores.py

--- p02/q08_top2_scores.py
+++ p02/q08_top2_scores.py
@@ -1,1 +1,14 @@
 
+sscore = {}
+def grades(name, score):
+    sscore[name] = score
+students = int(input("Enter Number of Students Here:"))
+x = 0
+while x < students:
+    n = input("Name: ")
+    s = input("Score: ")
+    grades(n,s)
+    x = x + 1
+print(max(sscore))
+sscore.pop(max(sscore))
+print(max(sscore))
